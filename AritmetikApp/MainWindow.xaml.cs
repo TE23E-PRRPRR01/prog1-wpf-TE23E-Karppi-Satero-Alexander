9d99b63677d6708f672f19122f6d183ed13d778b
Alexander Karppi Säterö <alexander.karppisatero@elev.ga.ntig.se>
2025-03-19 15:03:27 +0100

    Övat inför prov

---
+++ AritmetikApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AritmetikApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void KlickBeräkna(object sender, RoutedEventArgs e)
+    {
+        string tal1Text = txbTal1.Text.Trim();
+        string tal2Text = txbTal2.Text.Trim();
+
+        int tal1 = StringTillInt(tal1Text);
+        int tal2 = StringTillInt(tal2Text);
+        string matteTyp = txbOperator.Text;
+
+        if (matteTyp == "*")
+        {
+            lblResultat.Content = tal1 * tal2;
+        }
+        else
+        {
+
+        }
+
+    }
+
+    private int StringTillInt(string text)
+    {
+        int.TryParse(text, out int tal);
+        return tal;
+    }
+}
---
+++ Övning1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Övning1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickRäkna(object sender, RoutedEventArgs e)
+    {
+        bool lyckades = int.TryParse(txbVikt.Text, out int vikt); 
+        if (vikt <= 50 && lyckades == true)
+        {
+            lblSvar.Content = $"Brev som väger {vikt} g kostar 22 kr (1 frimärke)";     
+        }
+        else if (vikt <= 100 && lyckades == true)
+        {
+            lblSvar.Content = $"Brev som väger {vikt} g kostar 44 kr (2 frimärke)";     
+        }
+        else if (vikt <= 250 && lyckades == true)
+        {
+            lblSvar.Content = $"Brev som väger {vikt} g kostar 66 kr (3 frimärke)";     
+        }
+        else if (vikt <= 500 && lyckades == true)
+        {
+            lblSvar.Content = $"Brev som väger {vikt} g kostar 88 kr (4 frimärke)";     
+        }
+        else if (vikt <= 1000 && lyckades == true)
+        {
+            lblSvar.Content = $"Brev som väger {vikt} g kostar 132 kr (6 frimärke)";     
+        }
+        else if (vikt <= 2000 && lyckades == true)
+        {
+            lblSvar.Content = $"Brev som väger {vikt} g kostar 154 kr (7 frimärke)";     
+        }
+        else if (vikt > 2000 && lyckades == true)
+        {
+            lblSvar.Content = $"Brevet väger för mycket";     
+        }
+        else
+        {
+            lblSvar.Content = $"Ange gilltigt heltal";
+        }
+        
+    }
+}
